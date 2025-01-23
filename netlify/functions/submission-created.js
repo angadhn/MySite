1e75cdc0bc1da812285065305e1e17489db39c3b
Angadh Nanjangud <angadh.n@gmail.com>
2025-01-23 12:47:58 +0000

    js ditto

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,39 +1,24 @@
 const { BUTTONDOWN_API_KEY } = process.env;
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
   const email = JSON.parse(event.body).payload.email;
   console.log(`Received a submission: ${email}`);
 
-  try {
-    const response = await fetch("https://api.buttondown.email/v1/subscribers", {
-      method: "POST",
-      headers: {
-        Authorization: `Token ${BUTTONDOWN_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email }),
-    });
+  const response = await fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  });
 
-    let responseText = await response.text();
-    console.log("Response:", responseText);
+  let responseText = await response.text();
+  console.log("Response:", responseText);
 
-    if (!response.ok) {
-      return {
-        statusCode: response.status,
-        body: JSON.stringify({ error: responseText })
-      };
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Subscription successful" })
-    };
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Internal server error" })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
 };
