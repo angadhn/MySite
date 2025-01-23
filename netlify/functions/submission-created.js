b56d53b3d35c34ed087d99131e93eaa75c7e50c8
Angadh Nanjangud <angadh.n@gmail.com>
2025-01-23 09:12:18 +0000

    Newsletter subscription.

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,24 @@
+const { BUTTONDOWN_API_KEY } = process.env;
+import fetch from "node-fetch";
+
+exports.handler = async (event, context) => {
+  const email = JSON.parse(event.body).payload.email;
+  console.log(`Received a submission: ${email}`);
+
+  const response = await fetch("https://api.buttondown.com/v1/subscribers", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  });
+
+  let responseText = await response.text();
+  console.log("Response:", responseText);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
+};
